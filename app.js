390311798ecd1433b2010b2918ef128467d99e0c
williambutacu <florin.butacu@stud.umfcd.ro>
2020-12-23 14:13:32 +0200

    3.8*: Phonebook backend step8

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const morgan = require("morgan")
 const app = express();
 
 app.use(express.json())
-app.use(morgan("tiny"))
+
+morgan.token("content",function(req,res){
+   
+    return JSON.stringify(req.body)
+})
+
+
+app.use(morgan(":method  :status :res[content-length] - :response-time ms :content"))
 
 
 let persons=[
